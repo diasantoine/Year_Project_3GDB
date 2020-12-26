447e924d480893ba763c6ab96c72092b78d8775b
Shinohdi <62138018+Shinohdi@users.noreply.github.com>
2020-12-26 14:43:12 +0100

    RAS

---
+++ Assets/Script/Skill/ChargedDash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargedDash : skill
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void UsingSkill()
+    {
+        
+    }
+
+    public override void ChargingSkill()
+    {
+        base.ChargingSkill();
+    }
+
+    public override void EndUsing(Ray rayon)
+    {
+        
+    }
+}
