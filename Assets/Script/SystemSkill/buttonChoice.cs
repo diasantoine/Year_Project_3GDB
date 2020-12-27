cc901c5c69613cb43d619e8bb36a8b6c4e200be5
Shinohdi <62138018+Shinohdi@users.noreply.github.com>
2020-12-27 15:16:57 +0100

    Systeme choix compétence par vague

---
+++ Assets/Script/SystemSkill/buttonChoice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonChoice : MonoBehaviour
+{
+
+    [SerializeField] private skill buttonSkill;
+    [SerializeField] private bool skillOrUpgrade;
+
+    private choiceSkill CS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CS = GameObject.Find("SystemChoice").GetComponent<choiceSkill>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Choice()
+    {
+        Debug.Log("SkillChoisis");
+
+        for (int i = 0; i < CS.buttonList.Count; i++)
+        {
+            Destroy(CS.buttonList[i]);
+            
+        }
+
+        CS.buttonList.Clear();
+
+    }
+}
---
+++ Assets/Script/SystemSkill/choiceSkill.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class choiceSkill : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> skillItem;
+    public List<GameObject> buttonList;
+    [SerializeField] private GameObject Canvas;
+
+    public int nVague;
+    [SerializeField] private int vagueChoice;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nVague = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nVague >= vagueChoice)
+        {
+            int random = Random.Range(1, 4);
+            switch (random)
+            {
+                
+
+                case 1:
+                    InstantiateButton(new Vector2(0, 0));
+                    break;
+                case 2:
+                    InstantiateButton(new Vector2(80, 0));
+                    InstantiateButton(new Vector2(-80, 0));
+                    break;                                                       
+                                                                                 
+                case 3:
+                    InstantiateButton(new Vector2(0, 0));
+                    InstantiateButton(new Vector2(-175, 0));
+                    InstantiateButton(new Vector2(175, 0));
+                    break;
+            }
+
+            nVague = 1;
+        }
+    }
+
+    void InstantiateButton(Vector2 poz)
+    {
+        GameObject newChoice = Instantiate(skillItem[Random.Range(0, skillItem.Count + 1)]);
+        newChoice.transform.SetParent(Canvas.transform);
+        newChoice.GetComponent<RectTransform>().localPosition = poz;
+        buttonList.Add(newChoice);
+    }
+}
