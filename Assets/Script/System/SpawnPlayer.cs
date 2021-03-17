5d8b43f0e0350c5838e179ba14c3e2827033babe
Shinohdi <62138018+Shinohdi@users.noreply.github.com>
2021-03-17 16:11:42 +0100

    PoisonShield améliorer

--- Assets/Script/System/SpawnPlayer.cs
+++ Assets/Script/System/SpawnPlayer.cs
@@ -6,23 +6,11 @@ public class SpawnPlayer : MonoBehaviour
 {
 
     [SerializeField] private Transform player;
-    private spawnEnnemyBasique SEB;
-
-    [SerializeField] private List<Transform> spawnList;
 
     // Start is called before the first frame update
     void Start()
     {
         player.position = gameObject.transform.position;
-        SEB = GameObject.Find("SpawnSystem").GetComponent<spawnEnnemyBasique>();
-
-        if (spawnList.Count > 0)
-        {
-            for (int i = 0; i < spawnList.Count; i++)
-            {
-                SEB.spawnPoz.Add(spawnList[i]);
-            }
-        }
 
     }
 
