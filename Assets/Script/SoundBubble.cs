c4a842add40019beae248631306587d43817ad65
ArcaTsu <57213316+ArcaTsu@users.noreply.github.com>
2021-03-30 18:15:24 +0200

    Creation + Implémentation pour le Particle System du Bubbling Mud

---
+++ Assets/Script/SoundBubble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBubble : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string Bubbling_Mud = "";
+
+    public ParticleSystem ps;
+    // FMODUnity.RuntimeManager.PlayOneShot(Bubbling_Mud, transform.position);
+
+    private void Update()
+    {
+        if(ps.time >= ps.main.duration - 0.005f)
+        {
+            Debug.Log("test");
+            FMODUnity.RuntimeManager.PlayOneShot(Bubbling_Mud, transform.position);
+        }
+    }
+}
